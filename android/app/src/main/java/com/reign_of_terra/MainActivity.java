6fc4a7bb86189c93a0e142baf35287e76eef3eed
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2017-09-14 12:48:54 +0100

    initial setup

---
+++ android/app/src/main/java/com/reign_of_terra/MainActivity.java
@@ -0,0 +1,15 @@
+package com.reign_of_terra;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "reign_of_terra";
+    }
+}
