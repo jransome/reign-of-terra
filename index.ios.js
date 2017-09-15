6f539e6e9843468e53abc502e2eb704b4d7d87cf
chuk-chuk <yulia.v.litvin@gmail.com>
2017-09-15 13:29:21 +0100

    create App.js to dry up indexes for andriod & ios

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,1 @@
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
-import { StackNavigator } from 'react-navigation'
-
-import Map from './components/Map'
-import Login from './components/Login'
-
-const reign_of_terra = StackNavigator({
-  Login: { screen: Login },
-  Map: { screen: Map },
-});
-
-AppRegistry.registerComponent('reign_of_terra', () => reign_of_terra);
+import './App.js';
