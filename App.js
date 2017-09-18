791d07179f2b20035f7d507f3fc8a4b6a911d97b
chuk-chuk <yulia.v.litvin@gmail.com>
2017-09-18 13:26:22 +0100

    setup firebase, add data.json

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import * as firebase from 'firebase';
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { StackNavigator } from 'react-navigation'
@@ -5,6 +6,14 @@ import { StackNavigator } from 'react-navigation'
 import Map from './app/screens/Map'
 
 import Login from './app/screens/Login'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDp8aMYHboDQ5mgbWUzyJ8plmrfV5jDKSk",
+   authDomain: "reign-of-terra-a496c.firebaseapp.com",
+   databaseURL: "https://reign-of-terra-a496c.firebaseio.com",
+   storageBucket: "reign-of-terra-a496c.appspot.com"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const reign_of_terra = StackNavigator({
   Login: { screen: Login },
