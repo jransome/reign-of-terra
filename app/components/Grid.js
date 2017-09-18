32c841f916c267a5d600a748096c7d21ab471e3f
James Ransome <jamesransome@msn.com>
2017-09-18 14:29:58 +0100

    rendering hardcoded grid squares

--- app/components/Grid.js
+++ app/components/Grid.js
@@ -3,15 +3,23 @@ import React, { Component } from 'react';
 import MapView from 'react-native-maps';
 import Square from './Square.js';
 
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Button,
+  Switch,
+  TouchableOpacity
+} from 'react-native';
+
 const BOUNDARIES = {
   topLatitude: 51.580456223295826,
   bottomLatitude: 51.4445655655249,
   leftLongitude: -0.33370971429690144,
   rightLongitude: 0.18402099859372356,
 };
-
-let dx = 0.4;
-let dy = 0.4;
 
 let fakeCoordA = {
   latitude: 52,
@@ -28,8 +36,25 @@ let fakeCoordC = {
   longitude: 1,
 };
 
-let fakeArray = [fakeCoordA, fakeCoordB, fakeCoordC];
+let fakeCoordD = {
+  latitude: 52,
+  longitude: 1,
+};
 
+let fakeSquare1 = [fakeCoordA, fakeCoordB, fakeCoordC, fakeCoordD];
+
+let fakeCoordE = {
+  latitude: 52,
+  longitude: 2,
+};
+
+let fakeCoordF = {
+  latitude: 51,
+  longitude: 2,
+};
+
+let fakeSquare2 = [fakeCoordC, fakeCoordD, fakeCoordE, fakeCoordF];
+let squareAry = [fakeSquare1, fakeSquare2]
 
 export default class Grid extends Component {
   constructor(props) {
@@ -39,17 +64,21 @@ export default class Grid extends Component {
       currentSquare: null,
     }
   }
+
   createGrid() {
 
   }
+
   updateGrid() {
     this.setState(previous => ({}))
   }
 
-
   render() {
-    return (
-      <Square coordinates={fakeArray} fillColor={"blue"}/>
-    )
+    // var testAry = ['ben', 'james', 'ryan', 'yulia']
+    var grid = squareAry.map(function(squareCoords){
+      return <Square coordinates={squareCoords} fillColor={"blue"}/>;
+    })
+    console.log(grid);
+    return <View>{grid}</View>
   }
 }
--- app/components/Square.js
+++ app/components/Square.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import MapView from 'react-native-maps';
 
+const SQUARE_HEIGHT_WIDTH = 0.1;
+
 export default class Square extends Component {
 
   constructor(props) {
