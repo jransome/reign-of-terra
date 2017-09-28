1c7ab306f94c8ea97aefc2f3b39b39c3307eb65a
Ryan Davidson <daviryan@aol.com>
2017-09-28 18:33:44 +0100

    First tests passing to render app

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -1,30 +1,11 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.android.js';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-jest.mock('Linking', () => {
-
-  // we need to mock both Linking.getInitialURL()
-  // and Linking.getInitialURL().then()
-  const getInitialURL = jest.fn()
-  getInitialURL.mockReturnValueOnce({then: jest.fn()})
-
-  return {
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    openURL: jest.fn(),
-    canOpenURL: jest.fn(),
-    getInitialURL: getInitialURL
-  }
-})
-
-describe('Index', () => {
-  it('renders correctly', () => {
-    const tree = renderer.create(
-      <Index />
-    );
-  });
-})
+test('renders correctly', () => {
+  const tree = renderer.create(
+    <reign_of_terra />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,13 +1,11 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.ios.js';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 test('renders correctly', () => {
   const tree = renderer.create(
-    <HomePage />
+    <reign_of_terra />
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
