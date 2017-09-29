e8e586822ea5e137acb3c2c3b726f6252d6f87bf
Ryan Davidson <daviryan@aol.com>
2017-09-29 11:04:41 +0100

    Tests working

--- spec/HomePageSpec.js
+++
@@ -1,8 +0,0 @@
-export default function(spec) {
-  spec.describe('Home page,click button to start', function() {
-    spec.it('works', async function() {
-      await spec.press('Scene.button');
-      await spec.exists('NextScene')
-    });
-  });
-}
